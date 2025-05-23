e1fe1b1ec22fc0af37b43055834d89bb9b36b66c
Jongkyun <hjk326333@gmail.com>
2025-05-23 17:02:58 +0900

    QueryDSL - 동적 쿼리와 BooleanBuilder

--- src/main/java/com/playdata/study/repository/MemberRepositoryCustom.java
+++ src/main/java/com/playdata/study/repository/MemberRepositoryCustom.java
@@ -1,11 +1,22 @@
 package com.playdata.study.repository;
 
+import com.playdata.study.dto.SearchDto;
+import com.playdata.study.entity.Idol;
 import com.playdata.study.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepositoryCustom {
 
-    // JPA꺼 아님! 상속 안받음!
+    // JPA꺼 아니에요! 상속 안받았어요!
     List<Member> findByName(String name);
+
+    List<Idol> findIdolByCondition(SearchDto dto);
+
 }
+
+
+
+
+
+
